03554a921a3695f11e51f40154ed614dbf345e88
patri <patri@LAPTOP-KP81OH7D.niacc.edu>
2020-10-15 09:38:07 -0500

    I added this loop in class

--- public_html/LoopsPractice1.js
+++ public_html/LoopsPractice1.js
@@ -1,6 +1,12 @@
 /* 
  * Display the numbers from 1 to 10
  */
-set
+
 document.write("The numbers from 1 to 10 are: <br>");
 
+let counter = 1;
+while (counter <= 10){
+    document.write(counter + "<br>");
+    counter++; //counter = counter + 1
+}
+document.write("Goodbye!");
